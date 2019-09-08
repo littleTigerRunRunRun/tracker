2158645dba09f8ffc21b5524847e3581f81ac251
胡施宇 <894416038@qq.com>
2019-09-08 23:14:57 +0800

    feat(category): 类别实现增加

--- server/app/controller/category.js
+++ server/app/controller/category.js
@@ -5,26 +5,46 @@ const fs = require('fs')
 const path = require('path')
 
 class CategoryController extends Controller {
-  async list() {
-    const { ctx } = this
-    let data = {
-      list: []
-    }
-    data.list = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../store/category.json'), 'utf8', (err, data) => {
+  getCategory() {
+    return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../store/category.json'), 'utf8', (err, data) => {
       if (err) {
         console.error(err)
         return
       }
     }).toString())
+  }
+  async list() {
+    const { ctx } = this
+    let data = {
+      list: []
+    }
+    data.list = this.getCategory()
     ctx.body = data
   }
   async add() {
     const { ctx } = this
-    console.log(this)
+    let query = ctx.request.query
     let message = ''
+    let code = 200
+    let data = this.getCategory()
+    let id = -1
+    if (data.hash[query.name] !== undefined) {
+      message = '已添加过的类别名'
+      code = 500
+    } else {
+      data.list.push(query)
+      id = data.hash[query.name] = data.list.length - 1
+      fs.writeFile(path.resolve(__dirname, '../../store/category.json'), JSON.stringify(data),  function(err) {
+        if (err) {
+            return console.error(err)
+        }
+        console.log('写入成功')
+      })
+    }
     ctx.body = {
       message,
-      id: 1
+      code,
+      id
     }
   }
 }
