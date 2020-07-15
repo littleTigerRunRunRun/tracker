b78f9c51a5dcca4f632cc4a13c5697ea903c3bb8
shiyu.hu <894416038@qq.com>
2020-07-15 20:09:07 +0800

    feat(blur): 高斯模糊项目开启

---
+++ src/pieces/threejs/gaussianBlur/entry.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tracker-piece-entry">
+    <div ref="container" class="container" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GaussianBlur',
+  props: {},
+  data() {
+    return {
+    }
+  },
+  mounted() {
+
+  },
+  beforeDestroy() {
+  },
+  methods: {
+    onEnterEnd() {
+      // 完全进入且动画结束的钩子
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .tracker-piece-entry{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+  }
+</style>
